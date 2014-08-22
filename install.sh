de1ca63948b0d46a638390a815f7857815fe998f
dimcha <dk@dimcha.ru>
2014-08-22 20:20:56 +0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp etc/lxc/common.seccomp /etc/lxc/
+cp etc/lxc/jet9-centos-template.conf /etc/lxc/
+cp etc/lxc/lxc.conf /etc/lxc/
+
+cp usr/share/lxc/templates/lxc-jet9-centos /usr/share/lxc/templates/
+
